e304178faae39a34a86c63e4d86962013aec6150
Kamil Sikorski <kasikors@microsoft.com>
2017-06-29 13:27:26 -0700

    Added a join call unit test.

--- CSharp/Library/Microsoft.Bot.Builder.RealTimeMediaCalling.Tests/RegistrationUnitTests.cs
+++ CSharp/Library/Microsoft.Bot.Builder.RealTimeMediaCalling.Tests/RegistrationUnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Autofac;
 using Microsoft.Bot.Builder.Calling.ObjectModel.Contracts;
@@ -43,6 +44,7 @@ namespace Microsoft.Bot.Builder.RealTimeMediaCalling.Tests
             {
                 CallService = service;
                 CallService.OnIncomingCallReceived += OnIncomingCallReceived;
+                CallService.OnJoinCallReceived += OnJoinCallReceived;
                 CallService.OnCallCleanup += OnCallCleanup;
 
                 CorrelationId = service.CorrelationId;
@@ -72,6 +74,23 @@ namespace Microsoft.Bot.Builder.RealTimeMediaCalling.Tests
                 return Task.CompletedTask;
             }
 
+            private Task OnJoinCallReceived(RealTimeMediaWorkflow realTimeMediaWorkflow)
+            {
+                JObject mediaConfiguration;
+                using (var writer = new JTokenWriter())
+                {
+                    writer.WriteRaw("MediaConfiguration");
+                    mediaConfiguration = new JObject { { "Token", writer.Token } };
+                }
+
+                var joinCall = realTimeMediaWorkflow.Actions.FirstOrDefault() as JoinCallAppHostedMedia;
+                joinCall.MediaConfiguration = mediaConfiguration;
+                realTimeMediaWorkflow.NotificationSubscriptions = new NotificationType[]
+                    {NotificationType.CallStateChange, NotificationType.RosterUpdate};
+
+                return Task.CompletedTask;
+            }
+
             private Task OnCallCleanup()
             {
                 return Task.CompletedTask;
@@ -79,7 +98,7 @@ namespace Microsoft.Bot.Builder.RealTimeMediaCalling.Tests
         }
 
         [Test]
-        public async Task CreatingBotWithIRealTimeMediaServices()
+        public async Task CreatingBotWithIncomingCall()
         {
             var settings = new Mock<IRealTimeMediaCallServiceSettings>();
             settings.Setup(a => a.CallbackUrl).Returns(new Uri("https://someuri/callback"));
@@ -89,7 +108,7 @@ namespace Microsoft.Bot.Builder.RealTimeMediaCalling.Tests
                 settings.Object,
                 a => new RealTimeMediaBot(a),
                 a => new RealTimeMediaCall(a));
-            var bot = RealTimeMediaCalling.Container.Resolve<IRealTimeMediaBot>();
+            var bot = RealTimeMediaCalling.GetBot();
 
             Assert.NotNull(bot);
             Assert.NotNull(bot.RealTimeMediaBotService);
@@ -153,6 +172,83 @@ namespace Microsoft.Bot.Builder.RealTimeMediaCalling.Tests
             Assert.NotNull(service.GetCallForId("0b022b88-f255-4667-9335-2335f30ee8de"));
 
             var call3 = service.GetCallForId("0b022b88-f255-4667-9335-2335f30ee8de") as RealTimeMediaCall;
+            Assert.NotNull(call3);
+            Assert.IsNotEmpty(call3.CorrelationId);
+            Assert.IsNotEmpty(call3.CallId);
+            Assert.AreEqual(call3.CorrelationId, call3.CallService.CorrelationId);
+            Assert.IsTrue(call3.CallId.StartsWith(call3.CorrelationId));
+            Assert.AreNotEqual(call1, call3);
+            Assert.AreNotEqual(call2, call3);
+
+            // TODO: There is no cleanup task, as far as I can tell.
+        }
+
+        private class JoinCallRealTimeMediaBotService : RealTimeMediaBotService
+        {
+            public JoinCallRealTimeMediaBotService(ILifetimeScope scope, IRealTimeMediaCallServiceSettings settings)
+                : base(scope, settings)
+            {
+            }
+
+            protected override Task PlaceCall(HttpContent content, string correlationId)
+            {
+                return Task.CompletedTask;
+            }
+        }
+
+        [Test]
+        public async Task CreatingBotWithJoinCall()
+        {
+            var settings = new Mock<IRealTimeMediaCallServiceSettings>();
+            settings.Setup(a => a.CallbackUrl).Returns(new Uri("https://someuri/callback"));
+            settings.Setup(a => a.NotificationUrl).Returns(new Uri("https://someuri/notification"));
+
+            RealTimeMediaCalling.RegisterRealTimeMediaCallingBot<JoinCallRealTimeMediaBotService, RealTimeMediaCallService>(
+                settings.Object,
+                a => new RealTimeMediaBot(a),
+                a => new RealTimeMediaCall(a));
+            var bot = RealTimeMediaCalling.GetBot();
+
+            Assert.NotNull(bot);
+            Assert.NotNull(bot.RealTimeMediaBotService);
+            Assert.AreSame(typeof(RealTimeMediaBot), bot.GetType());
+
+            var service = bot.RealTimeMediaBotService as IInternalRealTimeMediaBotService;
+            var joinCall = new JoinCallAppHostedMedia() {JoinToken = "ABC"};
+            var correlationId1 = Guid.NewGuid().ToString();
+            var correlationId2 = Guid.NewGuid().ToString();
+
+            await service.JoinCall(joinCall, correlationId1);
+            Assert.AreEqual(1, service.Calls.Count);
+            Assert.NotNull(service.GetCallForId(correlationId1));
+            Assert.Null(service.GetCallForId(correlationId2));
+
+            var call1 = service.GetCallForId(correlationId1) as RealTimeMediaCall;
+            Assert.NotNull(call1);
+            Assert.IsNotEmpty(call1.CorrelationId);
+            Assert.IsNotEmpty(call1.CallId);
+            Assert.AreEqual(call1.CorrelationId, call1.CallService.CorrelationId);
+            Assert.IsTrue(call1.CallId.StartsWith(call1.CorrelationId));
+
+            await service.JoinCall(joinCall, correlationId1);
+            Assert.AreEqual(1, service.Calls.Count);
+            Assert.NotNull(service.GetCallForId(correlationId1));
+            Assert.Null(service.GetCallForId(correlationId2));
+
+            var call2 = service.GetCallForId(correlationId1) as RealTimeMediaCall;
+            Assert.NotNull(call2);
+            Assert.IsNotEmpty(call2.CorrelationId);
+            Assert.IsNotEmpty(call2.CallId);
+            Assert.AreEqual(call2.CorrelationId, call2.CallService.CorrelationId);
+            Assert.IsTrue(call2.CallId.StartsWith(call2.CorrelationId));
+            Assert.AreNotEqual(call1, call2);
+
+            await service.JoinCall(joinCall, correlationId2);
+            Assert.AreEqual(2, service.Calls.Count);
+            Assert.NotNull(service.GetCallForId(correlationId1));
+            Assert.NotNull(service.GetCallForId(correlationId2));
+
+            var call3 = service.GetCallForId(correlationId2) as RealTimeMediaCall;
             Assert.NotNull(call3);
             Assert.IsNotEmpty(call3.CorrelationId);
             Assert.IsNotEmpty(call3.CallId);
